bce4ca1bea5d3ec136cab4f8e33a5e8a4b6027f2
HAssAN-SSS <cruzacometas@gmail.com>
2023-10-15 20:39:12 +0100

    i wnat to complet the cours

--- _printf.c
+++ _printf.c
@@ -1,4 +1,15 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+char *plcType(const char *format, char *typePlc);
+char **storArgs(va_list args, char **argArray, char *typePlc);
+int stringHnadel(char **argArray, va_list args, int i);
+int plcCounter(const char *format, int plcCount);
 /**
  * _printf - printf clone
  * @format: format
